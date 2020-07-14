564ba79cff97242b7d8836481fb9f1d753c77200
asanlu <1925751694@qq.com>
2020-07-14 18:52:14 +0800

    add react-app-rewired

---
+++ src/pages/notfund/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Notfund extends Component {
+  render() {
+    return (
+      <div>
+        <h3> 404 ,pages Notfund!</h3>
+      </div>
+    )
+  }
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import App from '../pages/App'
+import Notfund from '@/pages/notfund'
 
 export default class RouteConfig extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class RouteConfig extends Component {
         <Switch>
           <Route path="/" exact component={App} />
           <Route path="/home" exact component={App} />
+          <Route component={Notfund} />
         </Switch>
       </Router>
     )
