aa0858d7c96bafbaf2b59f7ff9fd78bfba536388
asanlu <1925751694@qq.com>
2020-07-17 00:08:21 +0800

    add child route

--- src/pages/counter/counter.js
+++ src/pages/counter/counter.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Route, Link } from 'react-router-dom';
 
 import { incrementAction } from '@/actions';
 
@@ -11,8 +12,12 @@ class Counter extends Component {
       <div>
         <input type="text" />
         <button type="button" onClick={dispatchAddNum}>增加</button>
+        <button type="button"><Link to={`${this.props.match.path}/aa`}>到aa组件</Link></button>
+        <button type="button"><Link to={`${this.props.match.path}/bb`}>到bb组件</Link></button>
         <p style={{ color: 'red' }}>测试输出:{counter.num}</p>
         {console.log(this.props)}
+        <Route path={`${this.props.match.url}/aa`} render={() => <div>I'm aa render!</div>} />
+        <Route path={`${this.props.match.url}/bb`} render={() => <div>I'm bb component!</div>} />
       </div>
     );
   }
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import App from '@/pages/App';
-import Notfund from '@/pages/notfund';
-// import Counter from '@/pages/counter/counter';
 
 import { Provider } from 'react-redux';
 import store from '@/store';
 import asyncComponent from '@/utils/asyncComponent';
+
+import App from '@/pages/App';
+import Notfund from '@/pages/notfund';
+const Counter = asyncComponent(() => import('../pages/counter/counter'));
 
 export default class RouteConfig extends Component {
   render() {
@@ -16,7 +17,7 @@ export default class RouteConfig extends Component {
           <Switch>
             <Route path="/" exact component={App} />
             <Route path="/home" exact component={App} />
-            <Route path="/counter" exact component={asyncComponent(() => import('@/pages/counter/counter'))} />
+            <Route path="/counter" component={Counter} />
             <Route component={Notfund} />
           </Switch>
         </Router>
